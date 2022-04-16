82cae3d439b97177852f4270820b13f4e61e2256
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-04-17 00:17:54 +0600

    courses data added

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><small>Copyright 2022 © Learn Quran</small></p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDvbjF8xtY6qURLruxDz-qCKCB8H9fbak8",
+    authDomain: "learn-quran-3f0fa.firebaseapp.com",
+    projectId: "learn-quran-3f0fa",
+    storageBucket: "learn-quran-3f0fa.appspot.com",
+    messagingSenderId: "246559313561",
+    appId: "1:246559313561:web:64a4f3bdadead8a4d03208"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
